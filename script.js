8f1bc7e0d9925cda89dcf0b11e5062993b6fc363
Greg AI <greg@local>
2026-08-26 05:55:02 +0700

    Update Greg API 2026-08-26 05:55:02

--- script.js
+++ script.js
@@ -513,7 +513,7 @@ requestAnimationFrame(
 	drawCanvasBg
 );
 
-const API_URL = "https://bibliographic-lodge-empty-criticism.trycloudflare.com";
+const API_URL = "https://instance-machinery-becoming-warning.trycloudflare.com";
 
 const STORAGE_KEY =
 	"greg_ui_api_v6";
