2f93d02ab92357b9313ba167710d61400e088ed4
Greg AI <greg@local>
2026-08-26 19:59:14 +0700

    Update Greg API 2026-08-26 19:59:14

--- script.js
+++ script.js
@@ -513,7 +513,7 @@ requestAnimationFrame(
 	drawCanvasBg
 );
 
-const API_URL = "https://fcc-pdf-published-worker.trycloudflare.com";
+const API_URL = "https://wake-cheque-shut-input.trycloudflare.com";
 
 const STORAGE_KEY =
 	"greg_ui_api_v6";
