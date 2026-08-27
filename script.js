b94535d323b0d3a0ae141bfbeb70c3e9969a28e7
Greg AI <greg@local>
2026-08-27 13:35:35 +0700

    Update Greg API 2026-08-27 13:35:35

--- script.js
+++ script.js
@@ -592,7 +592,7 @@ requestAnimationFrame(
    API
    ======================================================== */
 
-const API_URL = "https://oxygen-clearing-caps-cutting.trycloudflare.com";
+const API_URL = "https://photographers-fair-entertaining-furthermore.trycloudflare.com";
 
 
 /* ========================================================
