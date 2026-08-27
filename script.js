d1a87facf1a9650bc690839e097ab4f988822f40
Greg AI <greg@local>
2026-08-27 11:25:42 +0700

    Update Greg API 2026-08-27 11:25:42

--- script.js
+++ script.js
@@ -592,7 +592,7 @@ requestAnimationFrame(
    API
    ======================================================== */
 
-const API_URL = "https://uni-bid-depot-itself.trycloudflare.com";
+const API_URL = "https://moves-les-gray-rendered.trycloudflare.com";
 
 
 /* ========================================================
