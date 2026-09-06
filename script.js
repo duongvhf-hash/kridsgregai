d7c3f5f674ab7bebd28eb46ab55f796101d13730
Greg AI <greg@local>
2026-09-06 19:35:03 +0700

    Update Greg API 2026-09-06 19:35:03

--- script.js
+++ script.js
@@ -592,7 +592,7 @@ requestAnimationFrame(
    API
    ======================================================== */
 
-const API_URL = "https://craps-settings-comparable-pensions.trycloudflare.com";
+const API_URL = "https://tied-corners-gets-ignored.trycloudflare.com";
 
 
 /* ========================================================
