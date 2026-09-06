fcd4dc334ec13905fcdc1587173c6e1c797b212c
Greg AI <greg@local>
2026-09-06 15:01:40 +0700

    Update Greg API 2026-09-06 15:01:40

--- script.js
+++ script.js
@@ -592,7 +592,7 @@ requestAnimationFrame(
    API
    ======================================================== */
 
-const API_URL = "https://anytime-caused-phrase-hang.trycloudflare.com";
+const API_URL = "https://eagles-noted-karl-itself.trycloudflare.com";
 
 
 /* ========================================================
