083dfabbb79b3840af6ba6cebf8a436df35e69aa
Greg AI <greg@local>
2026-09-13 07:47:40 +0700

    Update Greg API 2026-09-13 07:47:40

--- script.js
+++ script.js
@@ -592,7 +592,7 @@ requestAnimationFrame(
    API
    ======================================================== */
 
-const API_URL = "https://elsewhere-insights-none-whom.trycloudflare.com";
+const API_URL = "https://declined-registry-salaries-shade.trycloudflare.com";
 
 
 /* ========================================================
