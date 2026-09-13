3374a90c95789d6def9dd7031fe3c60447fe06a2
Greg AI <greg@local>
2026-09-13 14:25:31 +0700

    Update Greg API 2026-09-13 14:25:31

--- script.js
+++ script.js
@@ -592,7 +592,7 @@ requestAnimationFrame(
    API
    ======================================================== */
 
-const API_URL = "https://declined-registry-salaries-shade.trycloudflare.com";
+const API_URL = "https://pope-speed-mention-suitable.trycloudflare.com";
 
 
 /* ========================================================
